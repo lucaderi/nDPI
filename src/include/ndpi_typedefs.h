2f80a77f14046eca83f6d03de0934966fe7a14d6
Luca <deri@ntop.org>
2025-08-04 22:47:00 +0200

    Added comment

--- src/include/ndpi_typedefs.h
+++ src/include/ndpi_typedefs.h
@@ -105,7 +105,9 @@ typedef enum {
   1. Add a new flow alert key to the enum FlowAlertTypeEnum in include/ntop_typedefs.h
   2. Add the very same flow alert key to the table flow_alert_keys in scripts/lua/modules/alert_keys/flow_alert_keys.lua
   3. Add the risk to the array risk_enum_to_alert_type in src/FlowRiskAlerts.cpp
-
+  4. Create a new file in scripts/lua/modules/alert_definitions/flow/ with the new alert risk defined
+  5. Update scripts/lua/modules/alert_keys/flow_alert_keys.lua adding a new risk
+  
   Example: https://github.com/ntop/ntopng/commit/aecc1e3e6505a0522439dbb2b295a3703d3d0f9a
  */
 typedef enum {
