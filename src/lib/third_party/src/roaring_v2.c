56d7a268ed2dd803e1e21edba00669baae1e7e4e
Luca Deri <deri@ntop.org>
2025-12-15 12:26:34 +0100

    Win fixes

--- src/lib/third_party/src/roaring_v2.c
+++ src/lib/third_party/src/roaring_v2.c
@@ -1,6 +1,6 @@
 #include "ndpi_config.h"
 
-#ifdef USE_OLD_ROARING
+#if defined(USE_OLD_ROARING) || defined(WIN32)
 // !!! DO NOT EDIT - THIS IS AN AUTO-GENERATED FILE !!!
 // Created by amalgamation.sh on 2023-02-12T11:34:02Z
 
